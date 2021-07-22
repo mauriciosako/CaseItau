4a63c4af1efbec1b29491e65c19a6f7568faeed0
unknown <mauricio.sako@gmail.com>
2021-07-22 14:15:22 -0300

    teste commit branch

--- APITransf/src/main/java/br/com/sako/Startup.java
+++ APITransf/src/main/java/br/com/sako/Startup.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Startup {
 	//teste
+	//teste-branch
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 	}
